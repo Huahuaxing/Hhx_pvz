28d3c3e0457b30fd9d08cb8a8747af406a292d15
Huahuaxing <qinghua746448@outlook.com>
2023-09-20 12:51:55 +0800

    面试前更新

--- 源.cpp
+++ 源.cpp
@@ -4,15 +4,18 @@
 #include<time.h>
 #include "tools.h"
 
+#include<mmsystem.h>
+#pragma comment(lib, "winmm.lib")
+
 #define WIN_WIDTH 900
 #define WIN_HEIGHT 600
 
 enum { WAN_DOU, KUI_HUA, SHI_REN_HUA, ZHI_WU_COUNT };  //植物卡牌枚举
 
 IMAGE imgBG;  //定义背景图片
-IMAGE imgBAR;  //定义植物商店
+IMAGE imgBAR;  //定义植物栏
 IMAGE imgCards[ZHI_WU_COUNT];  //定义植物卡牌数组
-IMAGE *imgZhiWu[ZHI_WU_COUNT][20];  //指针数组，每个指针保存gif每一帧的地址
+IMAGE *imgZhiWu[ZHI_WU_COUNT][20];  //地图上的植物动态帧指针数组
 
 int curX, curY;  //当前选中的植物，在移动过程中的坐标
 int curZhiWu = 0;  //0：没有选中， 1：选择了第一种植物， 应用于拖动植物
@@ -28,11 +31,13 @@ struct sunshineBall {
 	int x, y;  //阳光球在飘落过程中的坐标位置（x不变）
 	int frameIndex;  //当前显示的图片帧序号
 	int destY;  //飘落的目标位置的y坐标
+	int timer;  //计时器
 	bool usageIndex; //是否在使用
 };
 
-struct sunshineBall balls[10];  //阳光池
-IMAGE imgSunshineBall[29];
+struct sunshineBall balls[10];  //阳光池，里面放十个阳光球
+IMAGE imgSunshineBall[29];  //阳光球动态帧数组
+int sunshine;
 
 bool fileExist(const char* name) {
 	FILE* fp = fopen(name, "r");
@@ -53,6 +58,8 @@ void InitGame() {
 
 	memset(map, 0, sizeof(map));  //初始化草地植物数组
 
+	memset(balls, 0, sizeof(balls));  //初始化阳光池
+
 	char name[64];  //保存植物卡牌的路径
 	for (int i = 0; i < ZHI_WU_COUNT; i++) {
 		sprintf_s(name, sizeof(name), "res/Cards/card_%d.png", i + 1);
@@ -71,19 +78,33 @@ void InitGame() {
 		}
 	}
 
+	sunshine = 150;  //初始化阳光值
 	curZhiWu = 0;
 
-	memset(balls, 0, sizeof(balls));  //初始化阳光池
-
+	//加载阳光球的图片帧
 	for (int i = 0; i < 29; i++) {
 		sprintf_s(name, sizeof(name), "res/sunshine/%d.png", i + 1);
 		loadimage(&imgSunshineBall[i], name);
 	}
 
 	//配置随机种子
+	srand(time(NULL));
 
-
+	//游戏窗口
 	initgraph(WIN_WIDTH, WIN_HEIGHT, 1);
+
+	//设置字体
+	LOGFONT f;
+	gettextstyle(&f);
+	f.lfHeight = 30;
+	f.lfWeight = 15;
+	strcpy(f.lfFaceName, "Segoe UI Black");
+	f.lfQuality = ANTIALIASED_QUALITY;  //抗锯齿效果
+	settextstyle(&f);
+	setbkmode(TRANSPARENT);
+	setcolor(BLACK);
+
+
 }
 
 void updateWindows() {
@@ -120,12 +141,42 @@ void updateWindows() {
 		putimagePNG(curX - img->getwidth() / 2, curY - img->getheight() / 2, imgZhiWu[curZhiWu - 1][0]);
 	}
 
+	//渲染阳光
+	int ballMAX = sizeof(balls) / sizeof(balls[0]);
+	for (int i = 0; i < ballMAX; i++) {
+		if (balls[i].usageIndex) {
+			IMAGE* img = &imgSunshineBall[balls[i].frameIndex];
+			putimagePNG(balls[i].x, balls[i].y, img);
+		}
+	}
+
+	char scoreText[8];
+	sprintf_s(scoreText, sizeof(scoreText), "%d", sunshine);
+	outtextxy(255, 67, scoreText);  //输出阳光值
+
 	EndBatchDraw();
+}
+
+void collectSunshine(ExMessage* msg) {
+	int count = sizeof(balls) / sizeof(balls[0]);
+	for (int i = 0; i < count; i++) {
+		int x = balls[i].x;
+		int y = balls[i].y;
+		int w = 79;
+		int h = 79;
+		if (msg->x >= x && msg->x <= x + w && msg->y > y && msg->y <= y + h && balls[i].usageIndex == true) {
+			balls[i].usageIndex = false;
+			sunshine += 25;
+			mciSendString("play res/sunshine.mp3", 0, 0, 0);
+		}
+	}
 }
 
 void userClick() {
 	ExMessage msg;
 	static int status = 0;
+
+	//选植物卡牌
 	if (peekmessage(&msg)) {
 		if (msg.message == WM_LBUTTONDOWN) {
 			if (msg.x > 327 && msg.x < 327 + 55 * ZHI_WU_COUNT && msg.y > 12 && msg.y < 89) {
@@ -134,6 +185,9 @@ void userClick() {
 				curZhiWu = index + 1;
 				curX = msg.x;
 				curY = msg.y;
+			}
+			else {
+				collectSunshine(&msg);
 			}
 
 		}
@@ -161,13 +215,13 @@ void userClick() {
 
 
 }
-
+//创建阳光
 void createSunshine() {
 	static int count = 0;
-	static int fre = 400;
+	static int fre = 200;
 	count++;
 	if (count >= fre) {
-		fre = 200 + rand() % 200;
+		fre = 100 + rand() % 100;
 		count = 0;
 
 		//从阳光池中取一个可以使用的
@@ -181,7 +235,26 @@ void createSunshine() {
 		balls[i].frameIndex = 0;
 		balls[i].x = 260 + rand() % (900 - 260);  //260 . 900
 		balls[i].y = 60;
-		balls[i].destY = 200 + (rand() % 4) * 90;//
+		balls[i].destY = 200 + (rand() % 4) * 90;
+		balls[i].timer = 0;
+	}
+}
+
+void updateSunshine() {
+	int ballMAX = sizeof(balls) / sizeof(balls[0]);
+	for (int i = 0; i < ballMAX; i++) {
+		if (balls[i].usageIndex) {
+			balls[i].frameIndex = (balls[i].frameIndex + 1) % 29;
+			if (balls[i].timer == 0) {
+				balls[i].y += 2;
+			}
+			if (balls[i].y >= balls[i].destY) {
+				balls[i].timer++;
+				if (balls[i].timer >= 100) {
+					balls[i].usageIndex = false;
+				}
+			}
+		}
 	}
 }
 
@@ -197,6 +270,9 @@ void updataGame() {
 			}
 		}
 	}
+
+	createSunshine();  //创建阳光
+	updateSunshine();  //更新阳光状态
 }
 
 void startMenu() {
@@ -241,7 +317,7 @@ int main(void) {
 	bool flag = true;
 
 	//启动菜单
-	//startMenu();
+	startMenu();
 
 	//游戏内循环
 	while (1) {
